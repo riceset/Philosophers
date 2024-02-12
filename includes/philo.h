cda3e2757a95b3de34ca6cc7cae47bd62e8cc1b9
komeno <tkomeno@student.42tokyo.jp>
2024-02-12 17:43:40 +0900

    chore: Add basic structures skeleton

--- includes/philo.h
+++ includes/philo.h
@@ -23,4 +23,32 @@ typedef struct s_rules t_rules;
 typedef struct s_philo t_philo;
 typedef struct s_dinner t_dinner;
 
+//STRUCTS
+
+struct s_rules
+{
+    int philo_count;
+    int lifespan;
+    int dining_duration;
+    int rest_duration;
+    int min_meals;
+};
+
+struct s_philo
+{
+    int id;
+    pthread_t thread;
+    time_t last_meal_time;
+    t_dinner *dinner;
+};
+
+struct s_dinner
+{
+	time_t start_time;
+	t_rules rules;
+  int exit_status;
+	t_philo philos[PHILO_MAX];
+	pthread_mutex_t forks[PHILO_MAX];
+};
+
 #endif
