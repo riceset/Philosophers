7c06b3729332489a041ff70eedb00d539f147d85
komeno <tkomeno@student.42tokyo.jp>
2024-02-12 19:04:15 +0900

    chore: Rewrite Makefile to include all .c files on compilation

--- build/files.mk
+++ build/files.mk
@@ -1,22 +1,5 @@
-FILES			=	main.c \
-						helpers/is_digit.c \
-						helpers/my_atoi.c \
-						input_validation/validators/incorrect_num_args.c \
-						input_validation/validators/wrong_num_philos.c \
-						input_validation/validators/not_only_digits.c \
-						input_validation/correct_input.c \
-						time/conversion_to_ms.c \
-						time/get_time_in_ms.c \
-						dinner/can_prepare_dinner.c \
-						dinner/set_dinner_start_time.c \
-						dinner/set_dinner_status.c \
-						dinner/set_dinner_rules.c \
-						dinner/can_initialize_forks.c \
-						dinner/initialize_philosophers.c \
-						error_handling/report_and_set_error.c
-
 SRC_DIR 	= sources
 OBJ_DIR		=	objects
 
-SRCS			=	$(addprefix $(SRC_DIR)/, $(FILES))
-OBJS			=	$(addprefix $(OBJ_DIR)/, $(SRCS:.c=.o))
+SRCS = $(shell find $(SRC_DIR) -type f -name '*.c')
+OBJS = $(SRCS:$(SRC_DIR)/%.c=$(OBJ_DIR)/$(SRC_DIR)/%.o)
