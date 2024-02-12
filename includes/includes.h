bb01482c43bb88c13408b55f7c8b8b0ed7a990f4
komeno <tkomeno@student.42tokyo.jp>
2024-02-12 17:21:21 +0900

    chore: Add initial file structure

---
+++ includes/includes.h
@@ -0,0 +1,14 @@
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <limits.h>
+#include <stdbool.h>
+
+#include <pthread.h>
+#include <sys/time.h>
+
+#endif
---
+++ includes/philo.h
@@ -0,0 +1,6 @@
+#ifndef PHILO_H
+#define PHILO_H
+
+#include "includes.h"
+
+#endif
