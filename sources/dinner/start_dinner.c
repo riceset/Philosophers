36d9fa1cfc215d5ecfe809c7b5b2c68cb0bd4cc5
komeno <tkomeno@student.42tokyo.jp>
2024-02-13 02:22:18 +0900

    feat: Get time when dinner starts

--- sources/dinner/start_dinner.c
+++ sources/dinner/start_dinner.c
@@ -20,6 +20,7 @@ void start_dinner(t_dinner *dinner)
   int i;
 
   i = 0;
+  dinner->start_time = get_time_in_ms();
   while (i < dinner->rules.philo_count)
   {
     if (pthread_create(&dinner->philos[i].thread, NULL, philo_routine, &dinner->philos[i]) != SUCCESS)
