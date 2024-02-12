22196a2dc821c5e19ecdfebb52fafbd8e7d2140c
komeno <tkomeno@student.42tokyo.jp>
2024-02-12 17:43:15 +0900

    chore: Add defines such as PHILO_MAX

--- includes/philo.h
+++ includes/philo.h
@@ -1,6 +1,26 @@
 #ifndef PHILO_H
 #define PHILO_H
 
-#include "includes.h"
+//DEFINES
+
+#define PHILO_MAX 200
+#define MIN_MEALS_NOT_SET (-1)
+
+//INCLUDES
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <limits.h>
+#include <stdbool.h>
+
+#include <pthread.h>
+#include <sys/time.h>
+
+//TYPEDEFS
+typedef struct s_rules t_rules;
+typedef struct s_philo t_philo;
+typedef struct s_dinner t_dinner;
 
 #endif
