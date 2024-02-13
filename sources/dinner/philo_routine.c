2c9f7b882aaa6220b1321fba9e2db268d051b32a
komeno <tkomeno@student.42tokyo.jp>
2024-02-13 10:27:01 +0900

    feat: Add macros and functions to simulate actions

--- sources/dinner/philo_routine.c
+++ sources/dinner/philo_routine.c
@@ -1,5 +1,39 @@
 #include "philo.h"
 
+typedef enum e_philo_status
+{
+  DEAD,
+  EATING,
+  SLEEPING,
+  THINKING,
+  TAKING_FORK
+} t_philo_status;
+
+#define MSG_DEAD "died"
+#define MSG_EATING "is eating"
+#define MSG_SLEEPING "is sleeping"
+#define MSG_THINKING "is thinking"
+#define MSG_TAKING_FORK "has taken a fork"
+
+void print_in_required_format(t_philo *philo, char *action)
+{
+  printf("%ld %d %s\n", get_time_in_ms() - philo->dinner->start_time, philo->id, action);
+}
+
+void print_philo_status(t_philo *philo, t_philo_status status)
+{
+	if (status == DEAD)
+		print_in_required_format(philo, MSG_DEAD);
+	else if (status == EATING)
+		print_in_required_format(philo, MSG_EATING);
+	else if (status == SLEEPING)
+		print_in_required_format(philo, MSG_SLEEPING);
+	else if (status == THINKING)
+		print_in_required_format(philo, MSG_THINKING);
+	else if (status == TAKING_FORK)
+		print_in_required_format(philo, MSG_TAKING_FORK);
+}
+
 void life_check_and_wait_for(t_philo *philo, time_t duration)
 {
   time_t time_to_wait_for;
@@ -10,22 +44,24 @@ void life_check_and_wait_for(t_philo *philo, time_t duration)
   while (get_time_in_ms() < time_to_wait_for)
   {
     //check if someone died
+    //check the time
+
     usleep(1000);
   }
 }
 
 void eat(t_philo *philo)
 {
-  (void) philo;
+  print_philo_status(philo, EATING);
 }
 
 void rest(t_philo *philo)
 {
-  (void) philo;
+  print_philo_status(philo, SLEEPING);
 }
 void think(t_philo *philo)
 {
-  (void) philo;
+  print_philo_status(philo, THINKING);
 }
 
 void *philo_routine(void *arg)
@@ -37,7 +73,9 @@ void *philo_routine(void *arg)
   i = 0;
   while (i < philo->dinner->rules.min_meals)
   {
-    printf("I am philo %d\n", philo->id);
+    eat(philo);
+    rest(philo);
+    think(philo);
     i++;
   }
   return (NULL);
