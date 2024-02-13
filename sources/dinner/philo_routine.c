c556104dfa23ceae2ff03d7914b03b125eefcbed
komeno <tkomeno@student.42tokyo.jp>
2024-02-13 09:33:46 +0900

    feat: Add the life_check_and_wait_for() function

--- sources/dinner/philo_routine.c
+++ sources/dinner/philo_routine.c
@@ -1,5 +1,19 @@
 #include "philo.h"
 
+void life_check_and_wait_for(t_philo *philo, time_t duration)
+{
+  time_t time_to_wait_for;
+
+  time_to_wait_for = get_time_in_ms() + duration;
+  
+  (void)philo;
+  while (get_time_in_ms() < time_to_wait_for)
+  {
+    //check if someone died
+    usleep(1000);
+  }
+}
+
 void eat(t_philo *philo)
 {
   (void) philo;
