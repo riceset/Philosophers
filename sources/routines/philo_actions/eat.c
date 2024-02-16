1a97e0ff8a31807cb54191c4614226c9aeeee9b6
komeno <tkomeno@student.42tokyo.jp>
2024-02-16 18:37:33 +0900

    feat: Print until the number of times to eat

--- sources/routines/philo_actions/eat.c
+++ sources/routines/philo_actions/eat.c
@@ -6,7 +6,7 @@
 /*   By: tkomeno <tkomeno@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 10:41:01 by tkomeno           #+#    #+#             */
-/*   Updated: 2024/02/16 10:41:03 by tkomeno          ###   ########.fr       */
+/*   Updated: 2024/02/16 18:33:31 by tkomeno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	eat(t_philo *philo)
 	take_forks(philo);
 	print_philo_status(philo, EATING);
 	life_check_and_wait(philo, philo->dinner->rules.dining_duration);
+	update_times_eaten_safely(&philo->times_eaten_mutex, &philo->times_eaten, philo->times_eaten + 1);
 	update_last_meal_time_safely(&philo->last_meal_time_mutex,
 		&philo->last_meal_time, get_time_in_ms());
 	release_forks(philo);
--- sources/routines/supervisor_routine.c
+++ sources/routines/supervisor_routine.c
@@ -6,7 +6,7 @@
 /*   By: tkomeno <tkomeno@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 10:40:50 by tkomeno           #+#    #+#             */
-/*   Updated: 2024/02/16 15:19:22 by tkomeno          ###   ########.fr       */
+/*   Updated: 2024/02/16 18:37:17 by tkomeno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,10 +28,20 @@ void	*supervisor_routine(void *arg)
 				- dinner->philos[i].last_meal_time >= dinner->rules.lifespan)
 			{
 				print_philo_status(&dinner->philos[i], DEAD);
-				update_stop_condition_safely(&dinner->stop_mutex, &dinner->stop, true);
+				update_stop_condition_safely(&dinner->stop_mutex, &dinner->stop,
+					true);
 				pthread_mutex_unlock(&dinner->philos[i].last_meal_time_mutex);
 				return (NULL);
 			}
+			if (dinner->rules.min_meals != (unsigned int)MIN_MEALS_NOT_SET)
+				if (check_times_eaten_safely(&dinner->philos[i].times_eaten_mutex,
+						&dinner->philos[i].times_eaten) > dinner->rules.min_meals)
+				{
+					update_stop_condition_safely(&dinner->stop_mutex, &dinner->stop,
+						true);
+					pthread_mutex_unlock(&dinner->philos[i].last_meal_time_mutex);
+					return (NULL);
+				}
 			pthread_mutex_unlock(&dinner->philos[i].last_meal_time_mutex);
 			i++;
 		}
